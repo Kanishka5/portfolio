5fc08f6011c821d764a2714ac1c9aec1afb7407b
kanishka <kanishan14@gmail.com>
2019-02-13 21:04:03 +0530

    mail link added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,24 +5,56 @@ export default class Footer extends Component {
   render() {
     return (
       <div style={footerstyle}>
-        <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center',width:'100%'}}>
-          <p style={{ color:'white'}}>WHAT’S NEXT?</p>
-          <h1 style={{ color:'white'}}>Let’s work together!</h1>
-          <p style={{ color:'white'}}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%"
+          }}
+        >
+          <p style={{ color: "white" }}>WHAT’S NEXT?</p>
+          <h1 style={{ color: "white" }}>Let’s work together!</h1>
+          <p style={{ color: "white" }}>
             If you’d like to talk about some exciting projects where you’d like
             me to help you with user experience, or have a chat about some cool
             visual design projects, I’ll be more than happy to work together!
           </p>
-          <img src={btn} />
-          <div style={{ borderTopStyle:'solid',borderColor:'#8300c4',marginTop:'14.7%'}}>
-            <ul style={{ display:'flex',justifyContent:"space-around"}}>
-                <li><a>DRIBBLE</a></li>
-                <li><a>BEHANCE</a></li>
-                <li><a>INSTAGRAM</a></li>
-                <li><a>TWITTER</a></li>
-                <li><a>LINKEDIN</a></li>
+          <a
+            href="mailto:someone@example.com?Subject=Hello%20again"
+            target="_top"
+          >
+            <img src={btn} />
+          </a>
+          <div
+            style={{
+              borderTopStyle: "solid",
+              borderColor: "#8300c4",
+              marginTop: "10.7%"
+            }}
+          >
+            <ul style={{ display: "flex", justifyContent: "space-around" }}>
+              <li style={{ padding:'2rem 2rem 0px 2rem' }}>
+                <a>DRIBBLE</a>
+              </li>
+              <li style={{ padding:'2rem 2rem 0 2rem' }}>
+                <a>BEHANCE</a>
+              </li>
+              <li style={{ padding:'2rem 2rem 0 2rem' }}>
+                <a>INSTAGRAM</a>
+              </li>
+              <li style={{ padding:'2rem 2rem 0 2rem' }}>
+                <a>TWITTER</a>
+              </li>
+              <li style={{ padding:'2rem 2rem 0 2rem' }}>
+                <a>LINKEDIN</a>
+              </li>
+              <li style={{ padding:'2rem 2rem 0 2rem',paddingLeft:'10%' }}>
+                <p>MADE WITH 2019</p>
+              </li>
             </ul>
-        </div>
+          </div>
         </div>
       </div>
     );
@@ -35,7 +67,7 @@ const footerstyle = {
   left: 0,
   paddingTop: 50,
   marginTop: 50,
-  backgroundImage: 'linear-gradient(221deg, #8300c4, #4c0092)'
+  backgroundImage: "linear-gradient(221deg, #8300c4, #4c0092)"
 };
 
 const footercomp = {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,50 +1,70 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
 
-export default class Navbar extends Component{
-    render(){
-        return(
-            <div className="navbar" style={ navstyle }>
-                <div className="logo" style={headerStyle}>
-                    <h1>LOGO </h1>
-                </div>
-                <nav className='navbar' >
-                    <ul className="header_lists" style={ tabsstyle }>
-                        <li style={ liststyle }><a href='/' style={{ textDecoration:'none'}} ><h2>HOME</h2></a></li>
-                        <li style={ liststyle }><a href='#project' style={{ textDecoration:'none'}} ><h2>PROJECT</h2></a></li>
-                        <li style={ liststyle }><a href='#about' style={{ textDecoration:'none'}} ><h2>ABOUT</h2></a></li>
-                        <li style={ liststyle }><a href='#about' style={{ textDecoration:'none'}} ><h2>HIRE ME</h2></a></li>
-                    </ul>
-                </nav>
-            </div>
-        );
-    }
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar" style={navstyle}>
+        <div className="logo" style={headerStyle}>
+          <h1>LOGO </h1>
+        </div>
+        <nav className="navbar">
+          <ul className="header_lists" style={tabsstyle}>
+            <li style={liststyle}>
+              <a href="/" style={{ textDecoration: "none" }}>
+                <h2>HOME</h2>
+              </a>
+            </li>
+            <li style={liststyle}>
+              <a href="#project" style={{ textDecoration: "none" }}>
+                <h2>PROJECT</h2>
+              </a>
+            </li>
+            <li style={liststyle}>
+              <a href="#about" style={{ textDecoration: "none" }}>
+                <h2>ABOUT</h2>
+              </a>
+            </li>
+            <li style={liststyle}>
+              <a
+                href="mailto:someone@example.com?Subject=Hello%20again"
+                target="_top"
+                style={{ textDecoration: "none" }}
+              >
+                <h2>HIRE ME</h2>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
 }
 
 const navstyle = {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: '10vh',
-    backgroundColor:'transparent',
-    paddingRight:'5%',
-}
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+  height: "10vh",
+  backgroundColor: "transparent",
+  paddingRight: "5%"
+};
 
 const headerStyle = {
-    display:'flex',
-    paddingLeft: '13%',
-}
+  display: "flex",
+  paddingLeft: "13%"
+};
 
 const tabsstyle = {
-    display:'flex',
-    flexDirection:'row',
-    alignItems:'flex-end',
-    JustifyContent:'space-between',
-    // marginRight:'10%'
-}
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "flex-end",
+  JustifyContent: "space-between"
+  // marginRight:'10%'
+};
 
 const liststyle = {
-    listStyle:'none',
-    marginLeft: '1.2rem',
-    marginRight: '1.2rem',
-}
+  listStyle: "none",
+  marginLeft: "1.2rem",
+  marginRight: "1.2rem"
+};
